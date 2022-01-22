8d7671b4e7ff455b4693e61db3f105c1d3978ff3
Aluneed <31174087+aluneed@users.noreply.github.com>
2022-01-23 01:15:17 +0800

    exercise

--- exercises/1.21.scm
+++ exercises/1.21.scm
@@ -2,3 +2,17 @@
 
 ;;; exercise 1.21 ;;;
 
+(define (smallestDivisor x) (
+    iterator 3 x
+))
+
+(define (iterator n x) (
+    cond
+        ((>= (* n n) x) x)
+        ((= (remainder x n) 0) n)
+        (else (iterator (+ n 2) x))
+))
+
+(smallestDivisor 199)
+(smallestDivisor 1999)
+(smallestDivisor 19999)
